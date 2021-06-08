0e2e9872b57e160e1a1d143acd3e93419626e446
MusigwaOAF <pacifique.musigwa@oneacrefund.org>
2021-06-08 18:02:01 +0200

    Updates

---
+++ precache-manifest.180183ca98b1295212305561489e19f5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38e7b8bc51cffa3b5c325feebd498bb",
+    "url": "/selfcare/index.html"
+  },
+  {
+    "revision": "f695fa0aef0c6768777f",
+    "url": "/selfcare/static/css/10.d12b2e6d.chunk.css"
+  },
+  {
+    "revision": "3a9185d80aeeacd28c61",
+    "url": "/selfcare/static/css/4.4e6480ac.chunk.css"
+  },
+  {
+    "revision": "b94dafca5884cd2be8fe",
+    "url": "/selfcare/static/css/5.23ec1170.chunk.css"
+  },
+  {
+    "revision": "bc7194ceb15989bd9c91",
+    "url": "/selfcare/static/css/8.14ca2a06.chunk.css"
+  },
+  {
+    "revision": "9d868ebc2c24956bf203",
+    "url": "/selfcare/static/css/9.d12b2e6d.chunk.css"
+  },
+  {
+    "revision": "82e6280010d30164454a",
+    "url": "/selfcare/static/css/main.4d4a53c8.chunk.css"
+  },
+  {
+    "revision": "059e3ff41e4fdcb2faa7",
+    "url": "/selfcare/static/js/0.2768e857.chunk.js"
+  },
+  {
+    "revision": "4b5a1cd2c020c4e0b9a6",
+    "url": "/selfcare/static/js/1.b6dc8c75.chunk.js"
+  },
+  {
+    "revision": "f695fa0aef0c6768777f",
+    "url": "/selfcare/static/js/10.90999f73.chunk.js"
+  },
+  {
+    "revision": "24beaf836369aa9752d3",
+    "url": "/selfcare/static/js/11.59ad660d.chunk.js"
+  },
+  {
+    "revision": "3a9185d80aeeacd28c61",
+    "url": "/selfcare/static/js/4.54939b91.chunk.js"
+  },
+  {
+    "revision": "ac72c819b7663194747bb249fcffbb24",
+    "url": "/selfcare/static/js/4.54939b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94dafca5884cd2be8fe",
+    "url": "/selfcare/static/js/5.1b235cc3.chunk.js"
+  },
+  {
+    "revision": "97840c3243e726ace0b9",
+    "url": "/selfcare/static/js/6.6c64ad37.chunk.js"
+  },
+  {
+    "revision": "3442b386b90fcf938cd6",
+    "url": "/selfcare/static/js/7.8b4f01f2.chunk.js"
+  },
+  {
+    "revision": "bc7194ceb15989bd9c91",
+    "url": "/selfcare/static/js/8.55ffae84.chunk.js"
+  },
+  {
+    "revision": "9d868ebc2c24956bf203",
+    "url": "/selfcare/static/js/9.46e04dcd.chunk.js"
+  },
+  {
+    "revision": "82e6280010d30164454a",
+    "url": "/selfcare/static/js/main.320cbd05.chunk.js"
+  },
+  {
+    "revision": "7ef2e1224121b5459d67",
+    "url": "/selfcare/static/js/runtime-main.6a8ba495.js"
+  }
+]);
